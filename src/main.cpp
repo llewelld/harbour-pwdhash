c63a512f79218a8e28a38648a8380715f3cb2c80
Robert Gerlach <khnz@gmx.de>
2014-03-24 19:14:38 +0100

    license fixed

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,10 @@
   Copyright (C) 2014 Robert Gerlach <khnz@gmx.de>
   All rights reserved.
 
-  You may use this file under the terms of BSD license as follows:
-
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of the Jolla Ltd nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
   ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -50,4 +42,3 @@ int main(int argc, char *argv[])
 
     return app->exec();
 }
-
